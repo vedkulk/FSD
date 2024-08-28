3047db3ef78ae784244e78cbda4f9dbcb4799c2f
vedkulk <kulkarnived3@gmail.com>
2024-08-28 11:20:35 +0530

    lab 7

--- Lab 7/main.js
+++ Lab 7/main.js
@@ -1,12 +1,30 @@
 const giveAlert = () => {
-    alert('hello')
+    alert('hello Alert')
 }
 const colorChange = () =>{
     const colors=['Red', 'Blue', 'Green', 'Cyan','Orange', 'Purple', 'Beige', 'Black', 'Violet', 'Purple', 'Brown']
     const index = Math.floor(Math.random() * colors.length)
     document.body.style.backgroundColor=colors[index];
 }
-const epilepsy = setInterval(colorChange, 1000)
+//const epilepsy = setInterval(colorChange, 100)
+const confirmBox = () => {
+    test=confirm("Confirm method ")
+    if(test===true){
+        console.log('True val')
+    }
+    else{
+        console.log('Untrue')
+    }
+}
+const promptBox = ()=>{
+    let check = prompt("Are you human! ")
+    if(check==='Yes'||check==='yes'){
+        alert('Welcome to the site')
+    }
+    else{
+        alert('Access denied. ')
+    }
+}
 document.write('<p class="name">Hello Harsh </p>')
 const loops = ()=>{
     for(let i=0;i<11;i++){
