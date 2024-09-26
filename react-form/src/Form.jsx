6b892a044e58511cf032fc3959cf1cef8341c8a6
vedkulk <kulkarnived3@gmail.com>
2024-09-26 11:53:11 +0530

    CSS added

--- react-form/src/Form.jsx
+++ react-form/src/Form.jsx
@@ -29,132 +29,171 @@ const Form = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Form submitted:', formData);
-    // Here you would typically send the data to a server
+    alert('Form submitted:');
   };
 
   return (
-    <div className="main">
-      <h1>College Registration Form</h1>
+    <div className="form-container">
+      <h1 className="form-title">College Registration Form</h1>
       <form onSubmit={handleSubmit}>
-        <p>Name: </p>
-        <input
-          type="text"
-          name="firstName"
-          placeholder='First Name'
-          value={formData.firstName}
-          onChange={handleInputChange}
-        />
-        <input
-          type="text"
-          name="lastName"
-          placeholder='Last Name'
-          value={formData.lastName}
-          onChange={handleInputChange}
-        />
-        <p>Date of Birth</p>
-        <input
-          type="date"
-          name="dateOfBirth"
-          value={formData.dateOfBirth}
-          onChange={handleInputChange}
-        />
-        <p>Email</p>
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={formData.email}
-          onChange={handleInputChange}
-        />
-        <p>Father's Name: </p>
-        <input
-          type="text"
-          name="fatherFirstName"
-          placeholder='First Name'
-          value={formData.fatherFirstName}
-          onChange={handleInputChange}
-        />
-        <input
-          type="text"
-          name="fatherLastName"
-          placeholder='Last Name'
-          value={formData.fatherLastName}
-          onChange={handleInputChange}
-        />
-        <p>Mother's Name: </p>
-        <input
-          type="text"
-          name="motherFirstName"
-          placeholder='First Name'
-          value={formData.motherFirstName}
-          onChange={handleInputChange}
-        />
-        <input
-          type="text"
-          name="motherLastName"
-          placeholder='Last Name'
-          value={formData.motherLastName}
-          onChange={handleInputChange}
-        />
-        <p>Gender: </p>
-        <select
-          name="gender"
-          value={formData.gender}
-          onChange={handleInputChange}
-        >
-          <option value="male">Male</option>
-          <option value="female">Female</option>
-        </select>
-        <p>Nationality: </p>
-        <input
-          type="text"
-          name="nationality"
-          placeholder='Nationality'
-          value={formData.nationality}
-          onChange={handleInputChange}
-        />
-        <p>Home Address: </p>
-        <input
-          type="text"
-          name="streetAddress"
-          placeholder='Street Address'
-          value={formData.streetAddress}
-          onChange={handleInputChange}
-        />
-        <br/>
-        <input
-          type="text"
-          name="city"
-          placeholder='City'
-          value={formData.city}
-          onChange={handleInputChange}
-        />
-        <input
-          type="text"
-          name="country"
-          placeholder='Country'
-          value={formData.country}
-          onChange={handleInputChange}
-        />
-        <p>Mobile Number</p>
-        <input
-          type="tel"
-          name="mobileNumber"
-          pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
-          value={formData.mobileNumber}
-          onChange={handleInputChange}
-        />
-        <p>In a few sentences, write down why you want to become a student of our College.</p>
-        <textarea
-          name="whyJoin"
-          rows={4}
-          cols={40}
-          value={formData.whyJoin}
-          onChange={handleInputChange}
-        />
-        <br/>
-        <input type="submit" value="Submit" />
+        <div className="form-group">
+          <label className="form-label">Name: </label>
+          <div className="name-inputs">
+            <input
+              type="text"
+              name="firstName"
+              placeholder='First Name'
+              value={formData.firstName}
+              onChange={handleInputChange}
+              className="form-input"
+            />
+            <input
+              type="text"
+              name="lastName"
+              placeholder='Last Name'
+              value={formData.lastName}
+              onChange={handleInputChange}
+              className="form-input"
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <label className="form-label">Date of Birth</label>
+          <input
+            type="date"
+            name="dateOfBirth"
+            value={formData.dateOfBirth}
+            onChange={handleInputChange}
+            className="form-input"
+          />
+        </div>
+        <div className="form-group">
+          <label className="form-label">Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={formData.email}
+            onChange={handleInputChange}
+            className="form-input"
+          />
+        </div>
+        <div className="form-group">
+          <label className="form-label">Father's Name: </label>
+          <div className="name-inputs">
+            <input
+              type="text"
+              name="fatherFirstName"
+              placeholder='First Name'
+              value={formData.fatherFirstName}
+              onChange={handleInputChange}
+              className="form-input"
+            />
+            <input
+              type="text"
+              name="fatherLastName"
+              placeholder='Last Name'
+              value={formData.fatherLastName}
+              onChange={handleInputChange}
+              className="form-input"
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <label className="form-label">Mother's Name: </label>
+          <div className="name-inputs">
+            <input
+              type="text"
+              name="motherFirstName"
+              placeholder='First Name'
+              value={formData.motherFirstName}
+              onChange={handleInputChange}
+              className="form-input"
+            />
+            <input
+              type="text"
+              name="motherLastName"
+              placeholder='Last Name'
+              value={formData.motherLastName}
+              onChange={handleInputChange}
+              className="form-input"
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <label className="form-label">Gender: </label>
+          <select
+            name="gender"
+            value={formData.gender}
+            onChange={handleInputChange}
+            className="form-select"
+          >
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label className="form-label">Nationality: </label>
+          <input
+            type="text"
+            name="nationality"
+            placeholder='Nationality'
+            value={formData.nationality}
+            onChange={handleInputChange}
+            className="form-input"
+          />
+        </div>
+        <div className="form-group">
+          <label className="form-label">Home Address: </label>
+          <input
+            type="text"
+            name="streetAddress"
+            placeholder='Street Address'
+            value={formData.streetAddress}
+            onChange={handleInputChange}
+            className="form-input"
+          />
+          <div className="address-inputs">
+            <input
+              type="text"
+              name="city"
+              placeholder='City'
+              value={formData.city}
+              onChange={handleInputChange}
+              className="form-input"
+            />
+            <input
+              type="text"
+              name="country"
+              placeholder='Country'
+              value={formData.country}
+              onChange={handleInputChange}
+              className="form-input"
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <label className="form-label">Mobile Number</label>
+          <input
+            type="tel"
+            name="mobileNumber"
+            pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+            value={formData.mobileNumber}
+            onChange={handleInputChange}
+            className="form-input"
+          />
+        </div>
+        <div className="form-group">
+          <label className="form-label">Why do you want to become a student of our College?</label>
+          <textarea
+            name="whyJoin"
+            rows={4}
+            value={formData.whyJoin}
+            onChange={handleInputChange}
+            className="form-textarea"
+          />
+        </div>
+        <input type="submit" value="Submit" className="form-submit" />
       </form>
     </div>
   );
